da7f6447128d5858d0bda1cc26afc7479db765a2
Rioba-Ian <riobafelix@gmail.com>
2023-05-03 02:04:34 +0300

    set up event listener for nav

--- script/app.js
+++ script/app.js
@@ -1,2 +1,12 @@
 "use strict";
 console.log("Rioba is using TS.");
+const navLinks = document.querySelectorAll(".links-section li");
+console.log(navLinks);
+function handleClick(e) {
+    const { target } = e;
+    if (target)
+        console.log(target.textContent);
+}
+navLinks.forEach((link) => {
+    link.addEventListener("click", (e) => handleClick(e));
+});
--- script/app.ts
+++ script/app.ts
@@ -1,1 +1,13 @@
 console.log("Rioba is using TS.");
+const navLinks = document.querySelectorAll(".links-section li");
+
+console.log(navLinks);
+
+function handleClick(e: Event) {
+  const { target } = e;
+  if (target) console.log((target as HTMLLIElement).textContent);
+}
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", (e) => handleClick(e));
+});
