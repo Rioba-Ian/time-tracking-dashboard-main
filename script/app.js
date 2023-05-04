37885ded3344c1270c4298b030b7e3d6829c7fc1
Rioba-Ian <riobafelix@gmail.com>
2023-05-04 14:17:31 +0300

    change content on click

--- script/app.js
+++ script/app.js
@@ -1,12 +1,58 @@
 "use strict";
-console.log("Rioba is using TS.");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+let state = "Daily";
 const navLinks = document.querySelectorAll(".links-section li");
-console.log(navLinks);
-function handleClick(e) {
-    const { target } = e;
-    if (target)
-        console.log(target.textContent);
+const cardRowTitle = document.querySelectorAll(".card-row p");
+const cardHours = document.querySelectorAll(".card-row--content h2");
+const cardHistory = document.querySelectorAll(".card-row--content span");
+function fetchData() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const res = yield fetch("../data.json");
+        const myData = yield res.json();
+        for (let i = 0; i < cardRowTitle.length; i++) {
+            cardRowTitle[i].textContent = myData[i].title;
+        }
+        navLinks.forEach((link) => {
+            link.addEventListener("click", (e) => {
+                const value = e.target.textContent;
+                if (value === "Weekly")
+                    state = value;
+                else if (value === "Monthly")
+                    state = value;
+                else if (value === "Monthly")
+                    state = value;
+                else
+                    state = "Daily";
+                switch (state) {
+                    case "Weekly":
+                        for (let i = 0; i < cardHours.length; i++) {
+                            cardHours[i].textContent = `${myData[i].timeframes.weekly.current} hrs`;
+                            cardHistory[i].textContent = `Last Week - ${myData[i].timeframes.weekly.previous}hrs`;
+                        }
+                        break;
+                    case "Monthly":
+                        for (let i = 0; i < cardHours.length; i++) {
+                            cardHours[i].textContent = `${myData[i].timeframes.monthly.current} hrs`;
+                            cardHistory[i].textContent = `Last Month - ${myData[i].timeframes.monthly.previous}hrs`;
+                        }
+                        break;
+                    default:
+                        for (let i = 0; i < cardHours.length; i++) {
+                            cardHours[i].textContent = `${myData[i].timeframes.daily.current} hrs`;
+                            cardHistory[i].textContent = `Yesterday - ${myData[i].timeframes.daily.previous}hrs`;
+                        }
+                        break;
+                }
+            });
+        });
+    });
 }
-navLinks.forEach((link) => {
-    link.addEventListener("click", (e) => handleClick(e));
-});
+fetchData();
--- script/app.ts
+++ script/app.ts
@@ -1,13 +1,83 @@
-console.log("Rioba is using TS.");
-const navLinks = document.querySelectorAll(".links-section li");
-
-console.log(navLinks);
-
-function handleClick(e: Event) {
-  const { target } = e;
-  if (target) console.log((target as HTMLLIElement).textContent);
+interface Timeframe {
+  current: number;
+  previous: number;
 }
 
-navLinks.forEach((link) => {
-  link.addEventListener("click", (e) => handleClick(e));
-});
+interface Timeframes {
+  daily: Timeframe;
+  weekly: Timeframe;
+  monthly: Timeframe;
+}
+
+interface Data {
+  title: string;
+  timeframes: Timeframes;
+}
+
+type myData = Data[];
+
+let state: string = "Daily";
+
+const navLinks = document.querySelectorAll(".links-section li");
+const cardRowTitle = document.querySelectorAll(".card-row p");
+const cardHours: NodeListOf<Element> = document.querySelectorAll(
+  ".card-row--content h2"
+);
+const cardHistory: NodeListOf<Element> = document.querySelectorAll(
+  ".card-row--content span"
+);
+
+async function fetchData() {
+  const res = await fetch("../data.json");
+  const myData: myData = await res.json();
+
+  for (let i = 0; i < cardRowTitle.length; i++) {
+    cardRowTitle[i].textContent = myData[i].title;
+  }
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      const value = (e.target as HTMLLIElement).textContent;
+
+      if (value === "Weekly") state = value;
+      else if (value === "Monthly") state = value;
+      else if (value === "Monthly") state = value;
+      else state = "Daily";
+
+      switch (state) {
+        case "Weekly":
+          for (let i = 0; i < cardHours.length; i++) {
+            cardHours[
+              i
+            ].textContent = `${myData[i].timeframes.weekly.current} hrs`;
+            cardHistory[
+              i
+            ].textContent = `Last Week - ${myData[i].timeframes.weekly.previous}hrs`;
+          }
+          break;
+        case "Monthly":
+          for (let i = 0; i < cardHours.length; i++) {
+            cardHours[
+              i
+            ].textContent = `${myData[i].timeframes.monthly.current} hrs`;
+            cardHistory[
+              i
+            ].textContent = `Last Month - ${myData[i].timeframes.monthly.previous}hrs`;
+          }
+          break;
+        default:
+          for (let i = 0; i < cardHours.length; i++) {
+            cardHours[
+              i
+            ].textContent = `${myData[i].timeframes.daily.current} hrs`;
+            cardHistory[
+              i
+            ].textContent = `Yesterday - ${myData[i].timeframes.daily.previous}hrs`;
+          }
+          break;
+      }
+    });
+  });
+}
+
+fetchData();
